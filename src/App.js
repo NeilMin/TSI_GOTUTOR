11c64fefc92d94e03a0606669ab80512c801a436
Gage M Fredricks <gmfredri@ucsd.edu>
2020-05-13 15:02:13 -0700

    Implement HTML

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Geisel from './images/Geisel.jpg'
+
+class Login extends React.Component {
+    render() {
+        return (
+            <html>
+            <div className="heroImage">
+                <div className="heroText">
+                    <h1>GoTutor! by TSI.</h1>
+                    <p>The tutor-focused solution.</p>
+                    <div className="g-signin2"
+                         data-onsuccess={this.onSignIn}
+                         data-theme="dark"
+                         data-width="300px"/>
+                </div>
+            </div>
+
+            <div className="footerText">
+                <span>This project is considered proprietary software of the following named individuals.</span><br />
+                <span>Reproduction of any code or design on this site is prohibited in accordance with the UCSD policy of Academic integrity.</span><br />
+                <span>This site should only be referenced for grading the assignment given in CSE 110 by instructor Gary Gillespie.</span><br />
+                <span>This software is not intended for use outside of the CSE 110 team. Use at your own caution.</span>
+            </div>
+            </html>
+        );
+    }
+
+    onSignIn(googleUser) {
+        let userProfile = googleUser.getBasicProfile();
+        console.log("onSingIn")
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    ///TODO: Fix this Return
+    return (<Login/>);
 }
 
 export default App;

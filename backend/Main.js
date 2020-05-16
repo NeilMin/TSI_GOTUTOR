3c69f0782409f77a5ca5cad2ce535684aad605bd
Gage M Fredricks <gmfredri@ucsd.edu>
2020-05-15 18:12:53 -0700

    Formatting fixes. Commenting of Main.js. Consolidate image folder. Convert to new UI. Redirect to appointment page.

--- backend/Main.js
+++ backend/Main.js
@@ -1,60 +1,67 @@
+// Cookie
+const cookieSession = require('cookie-session');
+
+// Express
 const express = require('express');
+const app = express();
+
+// HTTP
 const http = require('http');
-const cookieSession = require('cookie-session')
-var app = express();
-var httpServer = http.createServer(app);
+const httpServer = http.createServer(app);
+
+// Google Authentication
 const {OAuth2Client} = require('google-auth-library');
-const CLIENT_ID="294184945438-apd7ktfqguua67c4u06oq69sajq7nteq.apps.googleusercontent.com";
-const client = new OAuth2Client(CLIENT_ID);
+const CLIENT_ID = "294184945438-apd7ktfqguua67c4u06oq69sajq7nteq.apps.googleusercontent.com";
+const googleClient = new OAuth2Client(CLIENT_ID);
 
-app.use(function(req,res,next){
-    console.log(req.path+"requested");
+app.use(function (req, res, next) {
+    console.log(req.path + " requested");
     next();
-})
-
-
-
+});
 
 app.use(cookieSession({
     name: 's',
     secret: 'devel'
 }));
 
-app.get('/Success.html',function(req,res,next){
+app.get('/appointment.html', function (req, res, next) {
     console.log(req.session.uid);
     next();
-})
-app.use('/',express.static('old_site'));
+});
 
-app.post('/googleAuth',function(req,res){
-    let buffer="";
-    req.on('data',chunck=>{buffer+=chunck;});
-    req.on('end',async()=>{
-        //https://developers.google.com/identity/sign-in/web/backend-auth
+app.use('/', express.static('GOTUTOR_UI'));
+
+app.post('/googleAuth', function (req, res) {
+    let buffer = "";
+    req.on('data', chunk => {
+        buffer += chunk;
+    });
+    req.on('end', async () => {
+        // Reference: https://developers.google.com/identity/sign-in/web/backend-auth
         try {
-            const ticket = await client.verifyIdToken({
+            // No need to verify if the account is UCSD as the API is configured to only
+            // allow accounts that belong to the UCSD organization.
+            const ticket = await googleClient.verifyIdToken({
                 idToken: buffer,
-                audience: CLIENT_ID,  
+                audience: CLIENT_ID,
             });
+
+            // Get user information and put into JSON
             const payload = ticket.getPayload();
-            //console.log(payload);
-            if (payload['hd']!=='ucsd.edu') {
-                console.log(payload['hd'])
-                res.send(JSON.stringify({success:false,failedReason:"Please log in with UCSD google account"}));
-                return;
-            }
-            req.session.uid=payload['sub'];
-            req.sessionOptions.maxAge=payload['exp']*1e3-Date.now();
-            res.send(JSON.stringify({success:true,
-                userInfos:(({name,email,picture})=>({name,email,picture}))(payload)}));
+            req.session.uid = payload['sub'];
+            req.sessionOptions.maxAge = payload['exp'] * 1e3 - Date.now();
+            res.send(JSON.stringify({
+                success: true,
+                userInfos: (({name, email, picture}) => ({name, email, picture}))(payload)
+            }));
         } catch (error) {
             console.log(error);
-            res.send(JSON.stringify({success:false,failedReason:error}));
+            res.send(JSON.stringify({success: false, failedReason: error}));
         }
     })
 });
 
 
-httpServer.listen(80, function(){
+httpServer.listen(80, function () {
     console.log("Listening on port 80");
 });

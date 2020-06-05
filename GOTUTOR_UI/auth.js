27d5cb2390f69af2ea25663334fa61b8bd6f20d4
Gage M Fredricks <gmfredri@ucsd.edu>
2020-06-04 20:22:00 -0700

    Formatting, classroom navigation

--- GOTUTOR_UI/auth.js
+++ GOTUTOR_UI/auth.js
@@ -1,18 +1,43 @@
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      console.log('User signed out.');
-      document.cookie="";
-      sessionStorage.clear()
-      localStorage.clear()
-      window.location.replace("login.html")
+        console.log('User signed out.');
+        document.cookie = "";
+        sessionStorage.clear()
+        localStorage.clear()
+        window.location.replace("login.html")
     });
 }
-gapi.load('auth2', function() {
-var auth2 = gapi.auth2.init({client_id:"294184945438-qev9i7spuki27vk7lo7vs2cjpppi3rk9.apps.googleusercontent.com"}).then(function(auth2){
-if(!auth2.isSignedIn.get()&&!sessionStorage.getItem("testing")){
-  window.location.replace("login.html")
+
+function navigate(toNavigate) {
+    const isTutor = sessionStorage.getItem("isTutor") === "true";
+
+    if (toNavigate === 'assignment') {
+        if (isTutor) {
+            window.location.replace("tutorUI/writeup.html");
+        } else {
+            window.location.replace("studentUI/writeup.html");
+        }
+    } else if (toNavigate === 'classroom') {
+        if (isTutor) {
+            window.location.replace("tutorUI/classroom.html");
+        } else {
+            window.location.replace("studentUI/classroom.html");
+        }
+    } else if (toNavigate === 'appointment') {
+        if (isTutor) {
+            window.location.replace("tutorUI/appointment.html");
+        } else {
+            window.location.replace("studentUI/appointment.html");
+        }
+    }
 }
-});
+
+gapi.load('auth2', function () {
+    var auth2 = gapi.auth2.init({client_id: "294184945438-qev9i7spuki27vk7lo7vs2cjpppi3rk9.apps.googleusercontent.com"}).then(function (auth2) {
+        if (!auth2.isSignedIn.get() && !sessionStorage.getItem("testing")) {
+            window.location.replace("login.html")
+        }
+    });
 
 });
